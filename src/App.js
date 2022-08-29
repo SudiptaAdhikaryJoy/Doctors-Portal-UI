c7aa90b40df58633abe1e1b14d3214e2364a6a0b
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-08-29 23:35:19 +0600

    added daisyUI

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
-          {/* <Route path="home" element={<Home />} /> */}
+          <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login />} />
           <Route path="contact" element={<Contact />} />        
