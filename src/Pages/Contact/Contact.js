3e30b3fbb9d1ecc58a857dbf61a1531fd6f153e0
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-08-30 23:49:21 +0600

    add contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,15 +2,8 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                {/* <img src={chair} class="max-w-sm rounded-lg shadow-2xl" /> */}
-                <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
-                </div>
-            </div>
+        <div>
+            <h1>This is a contact</h1>
         </div>
     );
 };
