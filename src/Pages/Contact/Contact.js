b901ac894bc6b48a080449fd07ce91817940f83d
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-09-12 23:57:59 +0600

    added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <h1>This is a contacts!!</h1>
+            <h1>This is a contacts!!!</h1>
         </div>
     );
 };
