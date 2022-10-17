85926534fe9cb3ca809c8d516a0a87af6c2c9beb
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-10-17 23:48:25 +0600

    remove

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
         <div className="my-28">
             <div className="text-center">
                 <h3 className="text-primary text-xl font-bold uppercase">Our Services</h3>
-                <h2 className="text-4xl">Services We Provides!</h2>
+                <h2 className="text-4xl">Services We Provides</h2>
             </div>
         </div>
     );
