56104a0c9e9f11f240b878046659205da388475b
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-10-21 23:34:29 +0600

    login using firebase

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>this login</h1>
+            <h1>Login Using firebase!</h1>
         </div>
     );
 };
